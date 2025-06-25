bf91fcf4e545a60f60f3fc33e139d31667c503d6
dhenriquex <davi.araujo@alunos.ifsuldeminas.edu.br>
2025-06-25 15:39:44 -0300

    lista

--- assents/scripts/script.js
+++ assents/scripts/script.js
@@ -1,5 +1,6 @@
-
-
+const canvas = document.getElementById('avatarCanvas');
+const ctx =  canvas.getContext('2d');
+const status = document.getElementById('status');
 const imagemDB= {
     olhos:[],
     nariz:[],
@@ -18,3 +19,11 @@ const avatar = {
    franja:null,
     roupa:null,
 };
+const imagens ={
+    olho:['olhos1.png','olhos2.png'],
+    nariz:['olhos1.png','olhos2.png'],
+    boca:['olhos1.png','olhos2.png'],
+    cabelo:['olhos1.png','olhos2.png'],
+    franja:['olhos1.png','olhos2.png'],
+    orlha:['olhos1.png','olhos2.png'],
+}
