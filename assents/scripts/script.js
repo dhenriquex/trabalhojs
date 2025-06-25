8adf7ccec913c0bc9b903968e25a3558a72c6302
dhenriquex <davi.araujo@alunos.ifsuldeminas.edu.br>
2025-06-25 15:20:21 -0300

    INICIO

---
+++ assents/scripts/script.js
@@ -0,0 +1,20 @@
+
+
+const imagemDB= {
+    olhos:[],
+    nariz:[],
+    cabelo:[],
+    boca:[],
+    orelha:[],
+    franja:[],
+    roupa:[],
+};
+const avatar = {
+    olhos:null,
+    nariz:null,
+   cabelo:null,
+     boca:null,
+   orelha:null,
+   franja:null,
+    roupa:null,
+};
